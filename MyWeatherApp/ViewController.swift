ed1f524809e46fad372a499b8602226ba6e1f87b
Antonio Ochoa <antonhub00@gmail.com>
2020-11-30 01:59:13 -0600

    Faded background added

--- MyWeatherApp/ViewController.swift
+++ MyWeatherApp/ViewController.swift
@@ -16,6 +16,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var weatherImageView: UIImageView!
     @IBOutlet weak var descriptionLabel: UILabel!
     @IBOutlet weak var ErrorLabel: UILabel!
+    @IBOutlet weak var backgroundImageView: UILabel!
     
     var weatherManagerObj = WeatherManager()
     
@@ -25,6 +26,8 @@ class ViewController: UIViewController {
         
         searchTextField.delegate = self
         weatherManagerObj.delegate = self
+        
+        setGradientBackground()
     }
 
     
@@ -118,4 +121,18 @@ extension UIImageView {
         }
     }
     
+}
+
+extension ViewController {
+    func setGradientBackground() {
+        let colorTop =  UIColor(red: 102/255.0, green: 204/255.0, blue: 255.0/255.0, alpha: 1.0).cgColor
+        let colorBottom = UIColor(red: 255.0/255.0, green: 204/255.0, blue: 102/255.0, alpha: 1.0).cgColor
+        
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.colors = [colorTop, colorBottom]
+        gradientLayer.locations = [0.0, 1.0]
+        gradientLayer.frame = self.view.bounds
+        
+        self.view.layer.insertSublayer(gradientLayer, at:0)
+    }
 }
